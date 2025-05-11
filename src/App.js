f09046e83d8382d9a350669fc3b67bcf3aab7f0e
Jonathan Peek <jonathan.peek21@gmail.com>
2025-05-11 07:59:33 -0700

    Tabs Edits

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Tabs from "./components/Tabs"; // ⬅️ You just made this
+import Tabs from "./components/TabsComponents"; // ⬅️ You just made this
 import "./App.css";
 
 const projects = [
